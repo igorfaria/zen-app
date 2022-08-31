8098d07341245e2ef7c7b092938bc9fdcb3c8cae
Igor Faria <itfaria@gmail.com>
2022-08-31 18:43:21 +0100

    Added some words to the list of words that needed to be in evidence and reduced the number of children that wordWrapper returns

--- app/component/ZenItem.tsx
+++ app/component/ZenItem.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Dimensions, Text, View } from 'react-native'
+import { StyleSheet, Dimensions, Text, View, ImageBackground } from 'react-native'
 import React from 'react'
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withTiming, ZoomIn } from 'react-native-reanimated'
 import {Gesture, GestureDetector} from 'react-native-gesture-handler'
 import { useFonts } from 'expo-font'
 
 import { ZenItemType } from '../core/ZenItems'
 import { ZenImage } from '../component/ZenImage'
+import md5 from 'md5'
 
 const {height, width} = Dimensions.get('window')
 
@@ -14,8 +15,8 @@ const MAX_HEIGHT : number = height
 const TOUCH_SLOP : number = 5
 const TIME_TO_ACTIVATE_PAN : number = 400
 
-const FADE_HIDDEN : number = 0.25
-const FADE_NEXT : number = 0.35 // it must be different than FADE_HIDDEN
+const FADE_HIDDEN : number = 0.45
+const FADE_NEXT : number = 0.65 // it must be different than FADE_HIDDEN
 
 const ZenItem : Function = (props: any) : JSX.Element => {
 
@@ -30,38 +31,38 @@ const ZenItem : Function = (props: any) : JSX.Element => {
   const rotateX = useSharedValue(0)
   const rotateY = useSharedValue(0)
 
-  const rStyle = useAnimatedStyle(() => {
-    const rHeight : number = interpolate(
-      y.value,
-      [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
-      [MIN_HEIGHT, MAX_HEIGHT],
-      Extrapolate.CLAMP,
-    )
+  const rStyle = (itemContainer : boolean = false) => {
+    return useAnimatedStyle(() => {
+      const rHeight : number = interpolate(
+        y.value,
+        [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
+        [MIN_HEIGHT, MAX_HEIGHT],
+        Extrapolate.CLAMP,
+      )
 
-    const opacity : number = interpolate(
-      y.value,
-      [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT, (index + 1) * MAX_HEIGHT],
-      [FADE_NEXT, 0.5, FADE_HIDDEN],
-      Extrapolate.CLAMP,
-    )
+      const opacity : number = interpolate(
+        y.value,
+        [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT, (index + 1) * MAX_HEIGHT],
+        [FADE_NEXT, 0.8, FADE_HIDDEN],
+        Extrapolate.CLAMP,
+      )
 
-    if(opacity == 0.25){
-        onHidden(index)
-    }
+      if(opacity == FADE_HIDDEN) onHidden(index)
 
-    return {
-      height: rHeight,
-      opacity: opacity,
-      transform: [
-        {
-          perspective: 300,
-        },
-        {
-          rotateY: `${rotateY.value}deg`,
-        },
-      ]
-    }
-  })
+      const transformStyle = itemContainer 
+      ? {} 
+      : {  opacity: opacity, transform: [
+        { perspective: 300 },
+       // { rotateY: `${rotateY.value}deg` }
+      ]}
+
+      return {
+        height: rHeight,
+        ...transformStyle
+      }
+
+    })
+  }
 
   const gesture = Gesture.Pan()
     .onBegin(event => {
@@ -114,39 +115,80 @@ const ZenItem : Function = (props: any) : JSX.Element => {
   })
  
   const quoteFont : string = 'Montserrat-Light'
+  const wrapperFont : string = 'Montserrat-Regular'
 
   const customFontStyle : object = { paddingHorizontal: 5, fontFamily: fontsLoaded ? quoteFont : 'sans-serif' }
 
-  const usedKeys : object = []
+  const usedKeys : string[] = []
   const uniqueKey : Function = () : string => {
-    const generatedKey : string = '_' + Math.random().toString(36).substr(2, 9)
-    if(generatedKey in usedKeys) return uniqueKey()
-    return generatedKey
+    const generatedKey : string = Math.random().toString(36).substr(2, 9)
+    const generatedKey2 : string = Math.random().toString(36).substr(2, 9)
+    let finalKey : string[] | string = [generatedKey, generatedKey2]
+    if(Math.random () >= 0.5) finalKey.reverse()
+    finalKey = finalKey.join((Date.now()).toString())
+    finalKey = md5(finalKey)
+    if(finalKey in usedKeys) return uniqueKey()
+    usedKeys.push(finalKey)
+    return finalKey
   }
 
-  const youWrapper : Function = (words : string) : JSX.Element[] => {
-      const undefinedElement = [<></>] 
-      const wordsWrapped : JSX.Element[] = []
-      const spllitedWords = words?.split(' ') ?? undefined
+  const wordWrapper : Function = (words : string) : JSX.Element => {
+      const spllitedWords = (typeof words === 'string') ? words.split(' ') : undefined 
+      
+      const undefinedElement = <Text key={uniqueKey()}></Text>
+
       if(typeof spllitedWords === 'undefined') return undefinedElement
-      const key : string = uniqueKey()
-      spllitedWords.map(word => {
-          let styleYou = {...customFontStyle}
-          if(word.toLocaleLowerCase().trim() == 'you') styleYou = {...styleYou, fontWeight: '600'}
-          const props : object = {style: styleYou, key: key}
-          wordsWrapped.push(<Text {...props}>{word}</Text>)
-      })
-      return wordsWrapped
-  }
 
+      const wordsWrapped : any[] = []
+      spllitedWords.map(word => {
+          word = `${word} `
+          let styleYou = { ...customFontStyle }
+          let wrappedWord : string | JSX.Element = word
+          switch(word.toLocaleLowerCase().trim()) {
+            case 'you': case 'love': 
+            case 'believe': case 'heart':
+            case 'live': case 'life':
+            case 'success': case 'care':
+            case 'positive': case 'truth':
+            case 'truly': case 'best':
+            case 'future': case 'dream':
+            case 'power': case 'liberty':
+            case 'present': 
+              const wrapperFontStyle : object = (fontsLoaded) 
+              ? { fontFamily: wrapperFont }  
+              : { fontWeight: '700' }
+              styleYou = {...styleYou, ...wrapperFontStyle }
+              const props : object = {style: styleYou, key: uniqueKey()}
+              wrappedWord = <Text {...props}>{word}</Text>
+            break
+          }
+          wordsWrapped.push(wrappedWord)
+      })
+
+      return <Text key={uniqueKey()} style={customFontStyle}>{wordsWrapped.map(w => w)}</Text>
+  }
 
   return (
     <GestureDetector gesture={gesture}>
-      <Animated.View style={[styles.imageContainer, rStyle]}>
-        <ZenImage source={ { uri: item.image } } style={ styles.image } />
+      <Animated.View style={rStyle(true)}>
+
+      <ImageBackground 
+        style={ {
+          position: 'absolute',
+          left: 0, top: 0,
+          width: '100%',
+          height: height,
+          zIndex: 1,
+        } } 
+        resizeMode={'cover'}
+        source={ require('../assets/background/pattern-1.jpg') } />
+
+        <Animated.View style={[rStyle(false), {zIndex: 2, backgroundColor: 'black'}]}>
+          <ZenImage source={ { uri: item.image } } style={styles.image} />
+        </Animated.View>
         <View style={ styles.quoteContainer }>
           <Text style={ styles.quote }>
-              {youWrapper(item.quote)}
+              {wordWrapper(item.quote)}
           </Text>
         </View>
       </Animated.View>
@@ -163,9 +205,10 @@ const styles = StyleSheet.create({
     height: undefined,
     width: undefined,
     flex: 1,
+    zIndex: 1,
   },
   quote: {
-    backgroundColor: 'rgba(255,255,255,.55)',
+    backgroundColor: 'rgba(255,255,255,.6)',
     color: 'black',
     fontWeight: '300',
     fontSize: 32,
@@ -174,12 +217,14 @@ const styles = StyleSheet.create({
     elevation: 1,
     textAlign: 'center',
     flexDirection: 'row',
+    zIndex: 2
   },
   quoteContainer: {
-    backgroundColor: 'rgba(39, 122, 225,.5)',
+    backgroundColor: 'rgba(0, 0, 0,.5)',
     position: 'absolute',
     left: 0,
     top: 0,
+    zIndex: 2,
     width: '100%',
     height: '100%',
     justifyContent: 'center',
